3cb804937cfa616a882bdb14e0553ece143abe91
chandrashekharbankapur12-tech <chandrashekharbankapur12@gmail.com>
2026-03-08 09:28:39 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,7 @@
-const CACHE_NAME = 'toolbox-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png'
-];
-
-// Install Event
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
+  self.skipWaiting();
 });
 
-// Fetch Event (Offline support ke liye)
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => {
-      return res || fetch(e.request);
-    })
-  );
+  e.respondWith(fetch(e.request));
 });
-
